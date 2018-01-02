da084d6b3c553515a0d5c75145e1cd6bf12a81e8
Spb8Lighting <sylvain.guiblain@gmail.com>
2018-01-02 01:38:48 +0100

    Update

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+let gulp = require('gulp')
+,		$ = require('gulp-load-plugins')()
+,		EJSViews = ['views/*.ejs', 'views/pages/*.ejs', 'views/partials/*.ejs', 'views/pages/subpages/*.ejs']
+,		FolderJS = 	'dist/js/*.js'
+,		FolderCSS = 'dist/css/*.css'
+,		FolderSCSS = 'private/scss/*.scss'
+
+gulp.task('sass', () => {
+	return gulp.src(FolderSCSS)
+		.pipe($.sass({
+			onError: console.error.bind(console, 'SASS Error')
+		}))
+		.pipe(gulp.dest('dist/css'))
+})
+
+gulp.task('watch', () => {
+	let server = $.livereload()
+	gulp.watch(FolderSCSS, ['sass'])
+	gulp.watch([FolderJS, FolderCSS]).on('change', event => {
+		server.changed(event.path)
+	})
+})
+
+gulp.task('clean', () => {
+	return gulp.src('dist', {read : false}).pipe($.clean())
+})
+
+gulp.task('default', ['sass', 'clean'], () => {
+})
